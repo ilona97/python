3e95b67d2b06767ca96c6435878cd08d898fe903
Ilona <pulawskailona@gmail.com>
2017-11-13 12:27:35 +0100

    dodaje pliki: git.txt, python.py, print.py

---
+++ print.py
@@ -0,0 +1,65 @@
+"""
+print(wartość1, wartość2, wartość3, sep="separator", end="co na końcu")
+
+"""
+#znaki białe: spacje, tabulatory, przejścia do następnej lini - wszystko czego nie widać
+#znaki specjalne: 
+# \n - następna linia 
+# \t - tabulator
+# \" \' 
+# \f \v \b
+
+#Ćwiczonka:
+
+#1 wypisz "Super"
+
+
+#2 wypisz "Kubuś Puchatek" dowolnym sposobem
+
+
+#3 wypisz "Kubuś Puchatek" nie używając spacji w wartościach
+
+
+#4 wypisz "Kubuś Puchatek, misio" nie używając spacji w wartościach
+
+
+#5 wypisz "Jeden, Dwa, Trzy, " nie używając znaków białych i interpunkcyjnych w wartościach
+
+
+#6 wypisz "Kropka." nie używając znaków interpunkcyjnych w wartościach
+
+
+#7 wypisz "Jeden, Dwa, Trzy." nie używając znaków białych i interpunkcyjnych w wartościach
+
+
+#8 wypisz  "Koniec
+#           Kropka."
+# nie używając znaków białych i interpunkcyjnych w wartościach
+
+
+#9 wypisz "Jeden - misio
+#          Dwa - misiaczki."
+# nie używając znaków interpunkcyjnych w separatorach i
+
+
+#10 wypisz "Jeden - misio
+#           Dwa - misiaczki
+#           Kropka"
+# nie używając słowa "Kropka" w wartościach
+
+
+#11 wypisz "Jeden - misio
+#           Kropka."
+# nie używając znaku przejścia do następnej linii w separatorach i wartościach
+
+#12  wypisz "Wpadła gruszka do fartuszka
+#            a za gruszką dwa jabłuszka,
+#            a śliweczka wpaść nie chciała,
+#            bo śliweczka niedojrzała!"
+# nie używając słowa 'śliweczka' i wykrzyknika w wartościach
+
+#13 wypisz "Jadą, jadą misie, Śmieją im się pysie.
+#           Przyjechały do lasu, narobiły hałasu,
+#           Przyjechały do boru, narobiły rumoru."
+# nie używając w wartościach litery 'u'
+
---
+++ python.py
@@ -0,0 +1,77 @@
+
+
+#Program(ciąg instrukcji):
+
+#start
+#instrukcja 1
+#instrukcja 2
+#instrukcja 3
+#instrukcja sterująca a:
+    #instrukcja a1
+    #instrukcja a2
+#instrukcja 4
+#instrukcja 5
+#instrukcja sterująca b:
+    #instrukcja b1
+    #instrukcja b2
+    #instrukcja b3
+#instrukcja 6
+#stop
+
+
+"""
+Mamy do dyspozycji komputer i język programowania, żeby komputer zrobił dla nas to co byśmy chcieli.
+
+
+1. Napisanie programu:
+Chcemy aby komputer(procesor) coś dla nas zrobił(obliczył):
+a. musimy dokładnie wiedzieć co chcemy żeby zostało zrobione    (określenie problemu)
+b. ustalamy co ma być zrobione w naszym języku                  (sporządzamy algorytm rozwiązujący krok po kroku nasz problem)
+c. zapisujemy co ma być zrobione w języku programowania         (piszemy program na komputerze w języku Python, na podstawie sporządzonego algorytmu)
+
+2. Uruchomienie i wykonanie programu:
+Procesor nie rozumie języka programowania.
+Dlatego musimy przetłumaczyć to co zapisaliśmy w języku programowania na język maszynowy, który jest zrozumiały dla procesora.
+Do tego służy interpreter języka Python, który musimy zainstalować na komputerze.
+Uruchamiamy interpreter języka podając mu jako argument plik naszego programu(np: python program.py)
+    Cykl wykonania naszego programu:
+        a. interpreter pobiera (pierwszą/kolejną) instrukcję z naszego programu
+        b. interpreter tłumaczy instrukcje z języka Python na język maszynowy
+        c. procesor wykonuje instrukcje którą dostanie w języku maszynowym
+        d. powrót do punktu a.
+    Interpreter pobiera kolejne instrukcje, aż do momentu kiedy się skończą.
+    Instrukcje pobierane są po kolei, kolejność może być zmieniona poprzez użycie specjalnych instrukcji - sterujących.
+    Dzięki instrukcjom sterującym możemy wybierać jaka instrukcja zostanie wykonana.
+
+
+            My(wypisz na ekran napis "Super") -> język programowania(print("Super")) -> program
+
+            Program -> interpreter -> język maszynowy(010101001010111010101001110101011)
+
+            Język maszynowy -> procesor -> wynik(to co byśmy chcieli)
+
+
+"""
+
+### Instrukcje sterujące
+
+## if (warunek) instrukcjaI
+"""
+a. sprawdź(oblicz) warunek
+b. jeśli warunek prawdziwy(=true) wykonaj instrukcjaI
+c. jeśli warunek fałszywy(=false) przejdź do następnej instrukcji programu
+"""
+
+#Ćwiczonko numer pierwsze: zapisz algorytm wykonania instrukcji if/else
+
+#Ćwiczonko numer drugie: zapisz algorytm wykoniania instrukcji if/elif/else
+
+
+## while (warunek) instrukcjaW
+"""
+a. sprawdź warunek
+b. jeśli warunek prwadziwy wykonaj instrukcjaW i przejdź do punktu a.
+c. jeśli warunek fałszywy przejdź do następnej instrukcji programu
+"""
+
+#Ćwiczonko numer trzecie: zapisz algorytm wykoniania instrukcji while/else
