39f530bf217f48f1d4e410a0bd4fcfc9299c6f02
karol_ubuntu <93karol.p@gmail.com>
2017-11-13 18:40:32 +0100

    input.py - ćwiczonka

---
+++ input.py
@@ -0,0 +1,22 @@
+'''
+input("napis")
+
+'''
+
+# cwiczonko pierwsze:
+# Napisz kod ktory poprosi Cia o podanie IMIENIA i wypisze na ekran powitanie: "Hej &IMIE!"
+
+# cwiczonko drugie:
+# cwiczonko czwarte:
+# Napisz kod ktory poprosi Cia o podanie dowolnej liczby, a jako rezultat zwroci liczbe 100 razy wieksza.
+
+# cwiczonko trzecie:
+# Napisz kod ktory poprosi Cia o podanie liczby A i B, a nastapnie zwroci napisz: "&A + &B = &WYNIK"
+
+# cwiczonko czwarte:
+# Napisz kod ktory poprosi Cia o podanie IMIENIA, nazwy MIEJSCOWOSCI zamieszkania z KODEM pocztowym, oraz ulubionego OWOCU.
+#   potem wypisze na ekran: "Jestem &IMIE,
+#                            &MIEJSCOWOSC(&KOD) to moje miasto,
+#                            Moj ulubiony owoc to &OWOC."
+#
+#   korzystaj z separatorow i end'ow jak w cwiczeniach z print.
