1c48bf7735e21f0de1cefc88c49dc791ed4009a4
karol_ubuntu <93karol.p@gmail.com>
2017-11-15 16:20:55 +0100

    while.py - ćwiczonka

---
+++ while.py
@@ -0,0 +1,87 @@
+'''
+instrukcja sterująca while - pętla
+
+while warunek:      
+    instrukcja      
+else:               #else jest opcjonalne
+    instrukcja      
+
+
+'''
+
+# ćwiczonko 1.
+# wypisz na ekranie dziesięć razy 'kaboom'
+
+# ćwiczonko 2. 
+# Na ekranie wypisz pobraną ilość znaków '@'
+# np. dla x=5 #=> @@@@@
+
+# ćwiczonko 3.
+# Napisz program który cztery razy pobierze od Ciebie dowolną liczbę,
+# a następnie zwróci tę liczbę podniesioną do trzeciej potęgi.
+
+# ćwiczonko 4.
+# Napisz program, który trzy razy pobierze od Ciebie liczbę x
+# i za każdym razem po pobraniu tej liczby wypisze x małpek@ na ekranie.
+# np. dla x=3 #=> @@@
+#         x=5 #=> @@@@@
+#         x=2 #=> @@
+
+# ćwiczonko 5.
+# Napisz program który narysuje prostokąt o podanych długościach boków.
+# Przykładowy wynik dla a=3 i b=10:
+    #########
+    #########
+    #########
+
+
+# ćwiczonko 6.
+# Napisz program który wymieni koła w samochodzie:
+# Program pyta użytkownika ile kół trzeba wymienić.
+# Następnie program pyta użytkownika, ile śrub ma każde koło.
+# Potem program wymienia koła w samochodzie.
+
+# Przykładowy wynik działania programu:
+# Dzień dobry! Ile kół mamy wymienić? 4
+# Hmmm. A ile jest śrub w każdym kole? 5
+# To zaczynam zdejmować.
+# Zdejmuje koło nr. 1
+#   Odkręcam śrubę nr. 1    #do wcięcia użyj tabulatora
+#   Odkręcam śrubę nr. 2
+#   Odkręcam śrubę nr. 3
+#   Odkręcam śrubę nr. 4
+#   Odkręcam śrubę nr. 5
+# Zdejmuje koło nr. 2
+#   Odkręcam śrubę nr. 1
+#   .....
+# ...
+# To zaczynam zakładać.
+# Zakładam koło nr. 1
+#   Wkręcam śrubę nr. 1
+#   Wkręcam śrubę nr. 2
+#   Wkręcam śrubę nr. 3
+#   Wkręcam śrubę nr. 4
+#   Wkręcam śrubę nr. 5
+# Zakładam koło nr. 2
+#   Wkręcam śrubę nr. 1
+#   .....
+# ...
+# Proszę bardzo! Można ruszać :)
+
+
+
+#ćwiczonko 7.
+#Napisz program, który poprosi Cię o liczbę całkowitą nieparzystą(n) większą od dwóch.
+#Jeśli liczba nie będzie nieparzysta, to program wyświetli komunikat "Niewłaściwa liczba!",
+#   a potem jeszcze raz poprosi o liczbę nieparzystą większą od dwóch i tak do skutki,
+#Jeśli liczba będzie poprawna - wyświetli na ekranie napis "A oto nasza choineczka:"
+#Następnie program wypisze na ekran choinkę o podstawie n.
+
+#Przykład dla n=11:
+#Podaj liczbę nieparzystą większą od dwóch: 11
+     #
+    ###
+   #####
+  #######
+ #########
+###########
